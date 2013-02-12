5314e17f42d18205acb73636513c2784a3b25ca5
Max Bogue <max@maxbogue.com>
2013-02-12 12:33:45 -0500

    Now that it's small, move the main into the RandomForest class.

--- RandomForest.java
+++ RandomForest.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -76,6 +77,36 @@ public class RandomForest<D> {
             decisions.add(tree.decide(sample.choices));
         }  
         return decisions.mode();
+    }
+
+    /**
+     * Parse input data, and time the construction of a random forest.
+     */
+    public static void main(String[] args) throws Exception {
+
+        // Parse arguments.
+        int size = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+        int m = Integer.parseInt(args[2]);
+        String dataFile = args[3];
+
+        // Read samples and attrs from the file.
+        Map<String,List<String>> attrs = new HashMap<String,List<String>>();
+        List<Sample<String>> samples = RandomForestInput.readData(dataFile, attrs);
+
+        // Start timing.
+        long startTime = System.currentTimeMillis();
+
+        // Grow the forest.
+        RandomForest<String> forest = RandomForest
+            .<String>growRandomForest(attrs, samples, size, n, m);
+
+        // Stop timing.
+        long endTime = System.currentTimeMillis();
+
+        // Print results.
+        System.out.println((endTime - startTime) + " millis");
+
     }
 
 }
