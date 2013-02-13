a87c2180046085c06def6030e262a1b9fc198c39
Max Bogue <max@maxbogue.com>
2013-02-12 19:50:13 -0500

    Add split argument, data shuffling, and better output.

--- RandomForest.java
+++ RandomForest.java
@@ -87,13 +87,20 @@ public class RandomForest<D> {
         int n = Integer.parseInt(args[1]);
         int m = Integer.parseInt(args[2]);
         String dataFile = args[3];
+        double split;
+        if (args.length > 4) {
+            split = Double.parseDouble(args[4]) / 100.0;
+        } else {
+            split = 0.75;
+        }
 
         // Read samples and attrs from the file.
         Map<String,List<String>> attrs = new HashMap<String,List<String>>();
         List<Sample<String>> data = RandomForestInput.readData(dataFile, attrs);
+        data = ListUtils.shuffle(data);
 
         // Split into training and testing data.
-        int numTraining = (int)(data.size() * 0.75);
+        int numTraining = (int)(data.size() * split);
         List<Sample<String>> trainingData = data.subList(0, numTraining);
         List<Sample<String>> testData = data.subList(numTraining, data.size());
 
@@ -107,9 +114,6 @@ public class RandomForest<D> {
         // Stop timing.
         long t2 = System.currentTimeMillis();
 
-        // Print results.
-        System.out.println("Forest construction took " + (t2 - t1) + " ms");
-
         // Start timing.
         long t3 = System.currentTimeMillis();
 
@@ -120,10 +124,13 @@ public class RandomForest<D> {
         long t4 = System.currentTimeMillis();
 
         // Print results.
+        System.out.println(trainingData.size() + " samples used to train the forest.");
+        System.out.println(testData.size() + " samples used to test the forest.");
         double percent = 100.0 * correct / testData.size();
         System.out.printf("%.2f%% (%d/%d) tests passed.\n",
                 percent, correct, testData.size());
-        System.out.println("Forest testing took " + (t4 - t3) + " ms");
+        System.out.println("Forest construction time: " + (t2 - t1) + " ms");
+        System.out.println("Forest testing time: " + (t4 - t3) + " ms");
 
     }
 
